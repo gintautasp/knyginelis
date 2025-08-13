ba538f174168a30bd344d491d34a2fa3bcfcfaa7
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2025-08-13 09:42:50 +0300

    Sukurtas pradinis vaizdas su fono paveikslėliu

---
+++ main.html.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Knyginėlis</title>
+	<style>
+		body {
+			background: url(dusts/fonas-vasara.png);
+		}
+	</style>
+</head>
+<body>
+	<h1>Knyginėlis</h1>
+</body>
+</html>
---
+++ public/index.php
@@ -0,0 +1,3 @@
+<?php
+
+	include '../../projects/knygos/main.php';
